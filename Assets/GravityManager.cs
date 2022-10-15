7b461f966e38551eb0105ab7f38ed7358d94e313
cepRy <cepRytis@gmail.com>
2022-10-15 20:50:34 +0300

    gravitation accurate with real life formula

--- Assets/GravityManager.cs
+++ Assets/GravityManager.cs
@@ -1,14 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GravityManager : MonoBehaviour
 {
-    [SerializeField]
-    private float _pullRadiusFromCenter;
+    //[SerializeField]
+    private float _pullRadiusFromCenter = 20000;    //20000 temporary, all object should be pulled
 
     [SerializeField]
-    private float _pullForce;
+    private float _gravitationalConstant;
 
     // Start is called before the first frame update
     void Start()
@@ -26,13 +27,20 @@ public class GravityManager : MonoBehaviour
     public void FixedUpdate()
     {
         var collidersInsidePullRadius = Physics.OverlapSphere(transform.position, _pullRadiusFromCenter);
-        foreach (var collider in collidersInsidePullRadius)
+        foreach (var pulledCollider in collidersInsidePullRadius)
         {
             // calculate direction from target to me
-            Vector3 forceDirection = transform.position - collider.transform.position;
+            var forceDirection = transform.position - pulledCollider.transform.position;
+
+            var distanceBetweenCenters = Vector3.Distance(pulledCollider.transform.position, gameObject.transform.position);
+
+            var massPulledObject = pulledCollider.GetComponent<Rigidbody>().mass;
+            var massThisObject = gameObject.GetComponent<Rigidbody>().mass;
+
+            var forceToApply = (_gravitationalConstant * massThisObject * massPulledObject) / Math.Pow(distanceBetweenCenters, 2);
 
             // apply force on target towards me
-            collider.attachedRigidbody.AddForce(forceDirection.normalized * _pullForce * Time.fixedDeltaTime);
+            pulledCollider.attachedRigidbody.AddForce((float)forceToApply * forceDirection.normalized * Time.fixedDeltaTime);
         }
     }
 }
