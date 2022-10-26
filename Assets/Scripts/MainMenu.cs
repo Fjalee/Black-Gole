b29e7a7280261c2898194fe4d283b31f1383da44
cepRy <cepRytis@gmail.com>
2022-10-26 18:28:03 +0300

    link button to temporary scene

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("scene"); //temp change to level selection later
+    }
+}
