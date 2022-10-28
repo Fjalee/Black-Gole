a462f7b3aa76299a528f8e1b19f0002d7a083159
cepRy <cepRytis@gmail.com>
2022-10-28 12:09:01 +0300

    hide/disable buttons

---
+++ Assets/Scripts/UI/LevelSelectionHandle.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelectionHandle : MonoBehaviour
+{
+    [SerializeField]
+    List<int> _disabledButtonsIndexFromZero = new List<int>();
+    [SerializeField]
+    List<int> _hideButtonsIndexFromZero = new List<int>();
+
+    private void Start()
+    {
+        DisableButtonsFromList();
+        HideButtonsFromList();
+    }
+
+    private void DisableButtonsFromList()
+    {
+        var buttons = GetComponentsInChildren<Button>();
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            if (_disabledButtonsIndexFromZero.Contains(i))
+            {
+                buttons[i].interactable = false;
+            }
+        }
+    }
+
+    private void HideButtonsFromList()
+    {
+        var buttons = GetComponentsInChildren<Button>();
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            if (_hideButtonsIndexFromZero.Contains(i))
+            {
+                buttons[i].gameObject.SetActive(false);
+            }
+        }
+    }
+}
