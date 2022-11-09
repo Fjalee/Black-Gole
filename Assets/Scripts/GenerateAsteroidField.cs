4e6de317d66f896199fca4417e3da5f7dd50d557
Marius Kasparavičius <mariuskasparavicius@outlook.com>
2022-11-09 23:31:45 +0200

    Update second level and add collidable asteroids to it

--- Assets/Scripts/GenerateAsteroidField.cs
+++ Assets/Scripts/GenerateAsteroidField.cs
@@ -5,26 +5,23 @@ using UnityEngine;
 
 public class GenerateAsteroidField : MonoBehaviour
 {
-
     public Transform asteroidPrefab;
     public int fieldRadius = 100;
     public int asteroidCount = 100;
     public float scaleFromMultiplier = 0.5f;
     public float scaleToMultiplier = 5;
+    public float yAxisDivider = 3f;
 
     void Start()
     {
-
         for (int i = 0; i < asteroidCount; i++)
         {
-            Transform temp = Instantiate(asteroidPrefab, this.transform.position + Random.insideUnitSphere * fieldRadius, Random.rotation, this.transform); 
-            temp.localScale = temp.localScale * Random.Range(scaleFromMultiplier, scaleToMultiplier);
+            Vector3 randomPosition = Random.insideUnitSphere;
+            randomPosition.y /= yAxisDivider;
+
+            Transform temp = Instantiate(asteroidPrefab, transform.position + randomPosition * fieldRadius, Random.rotation, transform); 
+            temp.localScale = Vector3.one * Random.Range(scaleFromMultiplier, scaleToMultiplier);
         }
-
-    }
-
-    void Update()
-    {
 
     }
 }
