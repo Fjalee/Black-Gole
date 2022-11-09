6ca9d8bd429b6f723db8c2422d8d8f9e64752be4
cepRy <cepRytis@gmail.com>
2022-11-09 21:46:02 +0200

    formatting

--- Assets/Scripts/BallHit.cs
+++ Assets/Scripts/BallHit.cs
@@ -1,11 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BallHit : MonoBehaviour
 {
     [SerializeField]
     private SceneLoader.Scene _scene;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "PlanetBall")
