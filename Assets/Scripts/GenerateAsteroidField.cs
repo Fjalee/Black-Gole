3d069b2018b7ef1433b43fea384a6ed399e9e813
cepRy <cepRytis@gmail.com>
2022-11-10 09:02:46 +0200

    formatting

--- Assets/Scripts/GenerateAsteroidField.cs
+++ Assets/Scripts/GenerateAsteroidField.cs
@@ -1,26 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class GenerateAsteroidField : MonoBehaviour
 {
-    public Transform asteroidPrefab;
-    public int fieldRadius = 100;
-    public int asteroidCount = 100;
-    public float scaleFromMultiplier = 0.5f;
-    public float scaleToMultiplier = 5;
-    public float yAxisDivider = 3f;
+    [SerializeField]
+    private Transform _asteroidPrefab;
+
+    [SerializeField]
+    private int _fieldRadius = 100;
+
+    [SerializeField]
+    private int _asteroidCount = 100;
+
+    [SerializeField]
+    private float _scaleFromMultiplier = 0.5f;
+
+    [SerializeField]
+    private float _scaleToMultiplier = 5;
+
+    [SerializeField]
+    private float _yAxisDivider = 3f;
 
     void Start()
     {
-        for (int i = 0; i < asteroidCount; i++)
+        for (int i = 0; i < _asteroidCount; i++)
         {
             Vector3 randomPosition = Random.insideUnitSphere;
-            randomPosition.y /= yAxisDivider;
+            randomPosition.y /= _yAxisDivider;
 
-            Transform temp = Instantiate(asteroidPrefab, transform.position + randomPosition * fieldRadius, Random.rotation, transform); 
-            temp.localScale = Vector3.one * Random.Range(scaleFromMultiplier, scaleToMultiplier);
+            Transform temp = Instantiate(_asteroidPrefab, transform.position + randomPosition * _fieldRadius, Random.rotation, transform); 
+            temp.localScale = Vector3.one * Random.Range(_scaleFromMultiplier, _scaleToMultiplier);
         }
 
     }
