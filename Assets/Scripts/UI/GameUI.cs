dde2946191ee5ef70a09a6038defd1e953fed2f5
cepRy <cepRytis@gmail.com>
2022-11-12 19:08:06 +0200

    to observe level at the start

--- Assets/Scripts/UI/GameUI.cs
+++ Assets/Scripts/UI/GameUI.cs
@@ -1,9 +1,20 @@
+using System.Collections;
 using UnityEngine;
 
 public class GameUI : MonoBehaviour
 {
     [SerializeField]
     private Camera _cameraObserveLevel;
+
+    [SerializeField]
+    private int _secondsToObserveLevelAtStart = 2;
+
+    private IEnumerator ObserveLevelForTime(int secondsToWait)
+    {
+        _cameraObserveLevel.enabled = true;
+        yield return new WaitForSeconds(secondsToWait);
+        _cameraObserveLevel.enabled = false;
+    }
 
     public void RestartLevel()
     {
