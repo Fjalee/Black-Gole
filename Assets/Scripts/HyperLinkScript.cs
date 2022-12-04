6ef981961f0a92d23d06bd41387a8bd8d178db9c
JuozapasG <juozapas.gelcys@gmail.com>
2022-12-04 14:02:05 +0200

    scrollable assets text field, links to assets

---
+++ Assets/Scripts/HyperLinkScript.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HyperLinkScript : MonoBehaviour, IPointerClickHandler
+{
+    Dictionary<String, String> linkMap = new Dictionary<string, string>();
+
+    void Start()
+    {
+        linkMap.Add("BGM", "https://www.free-stock-music.com/search.php?keyword=space");
+        linkMap.Add("Planet_models", "https://assetstore.unity.com/packages/3d/environments/stylized-planet-pack-full-148233");
+        linkMap.Add("Skyboxes", "https://assetstore.unity.com/packages/2d/textures-materials/deep-space-skybox-pack-11056");
+        linkMap.Add("Asteroids", "https://assetstore.unity.com/packages/3d/environments/sci-fi/asteroids-low-poly-pack-142164");
+        linkMap.Add("Asteroids_sound", "https://mixkit.co/free-sound-effects/asteroid/");
+
+    }
+
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        var text = GetComponent<TextMeshProUGUI>();
+        int linkIndex = TMP_TextUtilities.FindIntersectingLink(text, Input.mousePosition, null);
+        if (linkIndex > -1)
+        {
+            var linkId = text.textInfo.linkInfo[linkIndex].GetLinkID();
+            Application.OpenURL(linkMap[linkId]);
+        }
+    }
+}
---
+++ Assets/Scripts/OpenLink.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+
+    Dictionary<String, String> linksMap = new Dictionary<String, String>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public OpenLink()
+    {
+        Debug.Log("open link");
+    }
+}
